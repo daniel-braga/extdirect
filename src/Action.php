97fe6f03d11c8f1cbba81cbd26b10f8a02039d4f
Daniel Braga <daniel.braga@shibata.com.br>
2015-11-16 10:58:54 -0200

    Allow result transformation. Added FormRequest/Upload handling.

--- src/Action.php
+++ src/Action.php
@@ -5,33 +5,38 @@ class Action implements ActionInterface
 {
     protected $name;
     protected $class;
-    protected $file;
+    protected $classFile;
     protected $method;
     protected $arguments = [];
     protected $tid;
-    protected $upload;
     protected $formHandler;
+    protected $files = [];
+    protected $resultTransformer;
 
     /**
      * Action constructor.
      *
-     * @param array $map
+     * @param array $classMap
      * @param $method
-     * @param $data
+     * @param $arguments
      * @param $tid
-     * @param bool|false $upload
      * @param bool|false $formHandler
+     * @param array $files
      */
-    public function __construct(array $map, $method, $arguments, $tid, $upload = false, $formHandler = false)
+    public function __construct(array $classMap, $method, $arguments, $tid, $formHandler = false, array $files = [])
     {
-        $this->name = $map['action'];
-        $this->class = $map['class'];
-        $this->file = $map['file'];
+        $this->name = $classMap['action'];
+        $this->class = $classMap['class'];
+        $this->classFile = $classMap['file'];
         $this->method = $method;
         $this->arguments = (array) $arguments;
         $this->tid = $tid;
-        $this->upload = $upload;
+        $this->files = $files;
         $this->formHandler = $formHandler;
+
+        if (isset($classMap['methods'][$method]['resultTransformer'])) {
+            $this->resultTransformer = $classMap['methods'][$method]['resultTransformer'];
+        }
     }
 
     /**
@@ -60,9 +65,63 @@ class Action implements ActionInterface
         return $response;
     }
 
+    public function isFormHandler()
+    {
+        return $this->formHandler;
+    }
+
+    public function isUpload()
+    {
+        return count($this->files) > 0;
+    }
+
+    public function hasFile($key)
+    {
+        return isset($this->files[$key]);
+    }
+
+    public function getFile($key)
+    {
+        if ($this->$this->hasFile($key)) {
+            return $this->files[$key];
+        }
+
+        throw new \OutOfBoundsException(sprintf('File upload key `%s` is invalid', $key));
+    }
+
     public function callAction()
     {
-        require_once $this->file;
-        return call_user_func_array(array($this->class, $this->method), $this->arguments);
+        Config::includeFile($this->classFile);
+
+        $reflectedMethod = new \ReflectionMethod($this->class, $this->method);
+
+        $arguments = [];
+        if ($this->isFormHandler()) {
+            /** @var \ReflectionParameter $reflectedParam */
+            foreach ($reflectedMethod->getParameters() as $reflectedParam) {
+                $paramName  = $reflectedParam->getName();
+                $paramValue = null;
+
+                if (isset($this->arguments[$paramName])) {
+                    $paramValue = $this->arguments[$paramName];
+                } elseif ($this->isUpload() && $this->hasFile($paramName)) {
+                    $paramValue = $this->getFile($paramName);
+                } elseif ($reflectedParam->isDefaultValueAvailable()) {
+                    $paramValue = $reflectedParam->getDefaultValue();
+                }
+
+                $arguments[] = $paramValue;
+            }
+        } else {
+            $arguments = $this->arguments;
+        }
+
+        $result = call_user_func_array(array($this->class, $this->method), $arguments);
+
+        if (is_callable($this->resultTransformer)) {
+            $result = call_user_func($this->resultTransformer, $this, $result);
+        }
+
+        return $result;
     }
 }
