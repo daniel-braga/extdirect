7902d225277011edbdf93a4cef1bec8f8983cde6
Daniel Braga <daniel.braga@shibata.com.br>
2015-11-19 09:53:19 -0200

    Add isFormHandler() and isUpload()

--- src/ActionInterface.php
+++ src/ActionInterface.php
@@ -7,4 +7,14 @@ interface ActionInterface
      * @return array
      */
     public function run();
+
+    /**
+     * @return boolean
+     */
+    public function isFormHandler();
+
+    /**
+     * @return boolean
+     */
+    public function isUpload();
 }
